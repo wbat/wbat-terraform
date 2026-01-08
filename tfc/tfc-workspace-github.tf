6d824df4d568f5ef34aca71bb5c21bd81a55d999
Brian Teller <bteller@mountain.com>
2026-01-07 23:39:43 -0500

    Fix TFC workspace configurations
    - Add trigger_patterns to preserve directory-based triggers
    - Move global_remote_state to tfe_workspace_settings (deprecated on workspace)
    - Use github_app_installation_id instead of oauth_token_id for VCS

--- tfc/tfc-workspace-github.tf
+++ tfc/tfc-workspace-github.tf
@@ -3,25 +3,25 @@ resource "tfe_workspace" "github" {
   description  = "WBAT's Github Workspace"
   organization = tfe_organization.wbat.id
 
-  auto_apply            = false
-  queue_all_runs        = false
-  global_remote_state   = true
+  auto_apply     = false
+  queue_all_runs = false
+
   terraform_version     = "1.14.3"
   working_directory     = "github"
   file_triggers_enabled = true
+  trigger_patterns      = ["/github"]
   ssh_key_id            = data.tfe_ssh_key.WBAT.id
 
   vcs_repo {
-    identifier         = "wbat/wbat-terraform"
-    ingress_submodules = "true"
-    branch             = "main"
-    oauth_token_id     = "ot-MNgANjF1qnpfxdry"
+    identifier                 = "wbat/wbat-terraform"
+    github_app_installation_id = "ghain-VDZVR9k8AKtDbiYa"
   }
 
   structured_run_output_enabled = true
 }
 
 resource "tfe_workspace_settings" "github" {
-  workspace_id   = tfe_workspace.github.id
-  execution_mode = "remote"
+  workspace_id        = tfe_workspace.github.id
+  execution_mode      = "remote"
+  global_remote_state = true
 }
