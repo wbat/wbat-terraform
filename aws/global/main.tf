12a0230b971cec625fb805fbb5601b0d1d4e3d79
Brian Teller <bteller@mountain.com>
2026-01-07 23:39:43 -0500

    Fix TFC workspace configurations
    - Add trigger_patterns to preserve directory-based triggers
    - Move global_remote_state to tfe_workspace_settings (deprecated on workspace)
    - Use github_app_installation_id instead of oauth_token_id for VCS

--- aws/global/main.tf
+++ aws/global/main.tf
@@ -23,7 +23,7 @@ module "cloudfront" {
   source = "./cloudfront"
 
   core_tags                = var.core_tags
-  origin_fqdn              = module.route53.origin_fqdn
   acm_certificate_arn      = module.acm.www_tellerstech_certificate_arn
   cloudfront_origin_secret = var.cloudfront_origin_secret
+  # origin_fqdn defaults to origin.tellerstech.com (managed in BIND)
 }
--- tfc/tfc-workspace-tfc.tf
+++ tfc/tfc-workspace-tfc.tf
@@ -3,25 +3,25 @@ resource "tfe_workspace" "tfc" {
   description  = "WBAT's Terraform Cloud Workspace"
   organization = tfe_organization.wbat.id
 
-  auto_apply            = false
-  queue_all_runs        = false
-  global_remote_state   = true
+  auto_apply     = false
+  queue_all_runs = false
+
   terraform_version     = "1.14.3"
   working_directory     = "tfc"
   file_triggers_enabled = true
+  trigger_patterns      = ["/tfc"]
   ssh_key_id            = data.tfe_ssh_key.WBAT.id
 
   vcs_repo {
-    identifier         = "wbat/wbat-terraform"
-    ingress_submodules = "true"
-    branch             = "main"
-    oauth_token_id     = "ot-MNgANjF1qnpfxdry"
+    identifier                 = "wbat/wbat-terraform"
+    github_app_installation_id = "ghain-VDZVR9k8AKtDbiYa"
   }
 
   structured_run_output_enabled = true
 }
 
 resource "tfe_workspace_settings" "tfc" {
-  workspace_id   = tfe_workspace.tfc.id
-  execution_mode = "remote"
+  workspace_id        = tfe_workspace.tfc.id
+  execution_mode      = "remote"
+  global_remote_state = true
 }
