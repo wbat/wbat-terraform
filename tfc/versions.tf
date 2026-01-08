6565995da36446124e0beec8f85162e8ab6c9428
Brian Teller <bteller@mountain.com>
2026-01-07 22:40:19 -0500

    Upgrade Terraform and provider versions
    - Terraform: 1.7.2 -> 1.14.3 (workspaces), >= 1.10 (constraints)
    - AWS provider: ~>4.0 -> ~>5.0
    - GitHub provider: ~>5.0 -> ~>6.0
    - TFE provider: add ~>0.60 constraint
    - GitHub Actions: checkout v2->v4, setup-terraform v1->v3

--- tfc/versions.tf
+++ tfc/versions.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~>5.0"
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "~>0.60"
     }
   }
   required_version = ">= 1.10"
