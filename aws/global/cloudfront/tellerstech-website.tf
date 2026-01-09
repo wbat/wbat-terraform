c97bf2edee8a5aa3962e46180ab2931e0239d700
Brian Teller <bteller@mountain.com>
2026-01-09 10:49:15 -0500

    Change sitemap caching from disabled to 2-hour TTL
    Sitemaps don't need real-time freshness like RSS feeds.
    Use WordPress cache policy (2hr default) instead of CachingDisabled.

--- aws/global/cloudfront/tellerstech-website.tf
+++ aws/global/cloudfront/tellerstech-website.tf
@@ -221,7 +221,7 @@ resource "aws_cloudfront_distribution" "tellerstech_website" {
     origin_request_policy_id = "216adef6-5c7f-47e4-b989-5492eafa07d3" # AWS Managed AllViewer
   }
 
-  # Sitemaps - no caching (must be fresh for search engines)
+  # Sitemaps - short cache (2 hour default like WordPress pages)
   ordered_cache_behavior {
     path_pattern             = "*.xml"
     allowed_methods          = ["GET", "HEAD"]
@@ -229,8 +229,8 @@ resource "aws_cloudfront_distribution" "tellerstech_website" {
     target_origin_id         = "wordpress-origin"
     viewer_protocol_policy   = "redirect-to-https"
     compress                 = true
-    cache_policy_id          = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" # AWS Managed CachingDisabled
-    origin_request_policy_id = "216adef6-5c7f-47e4-b989-5492eafa07d3" # AWS Managed AllViewer
+    cache_policy_id          = aws_cloudfront_cache_policy.wordpress.id
+    origin_request_policy_id = aws_cloudfront_origin_request_policy.wordpress.id
   }
 
   # Static assets - long cache TTL
