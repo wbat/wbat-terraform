3e076ff55ea99c97d8edd2832f008a7d3317e719
Brian Teller <bteller@mountain.com>
2026-01-10 18:11:09 -0500

    Move import blocks to dedicated imports.tf

--- aws/main.tf
+++ aws/main.tf
@@ -1,24 +1,4 @@
 ######################################################
-# Imports
-# After successful import, comment out or remove these blocks
-######################################################
-# import {
-#   to = module.global.module.cloudfront.aws_cloudfront_distribution.cdn_legacy[0]
-#   id = "E1BJFU3JD7PL7F"
-# }
-
-# EC2 Instances - importing existing "pet" servers into Terraform management
-import {
-  to = module.us-east-1.module.ec2.aws_instance.primary
-  id = "i-0572702f0a58f6dcd"
-}
-
-import {
-  to = module.us-east-1.module.ec2.aws_instance.secondary
-  id = "i-07e68eaf6cad1b838"
-}
-
-######################################################
 # Backend
 ######################################################
 terraform {
