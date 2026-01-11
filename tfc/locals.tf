e3056e8e32bc4562cd857182dc1fd74cfe5f02f9
Brian Teller <bteller@mountain.com>
2026-01-11 13:58:53 -0500

    Exclude TFC workspace from email variable set (circular dependency)

--- tfc/locals.tf
+++ tfc/locals.tf
@@ -6,4 +6,11 @@ locals {
     tfe_workspace.tfc.id,
   ]
 
+  # Workspaces that receive the email variable set
+  # Excludes TFC because it manages the variable set (circular dependency)
+  email_varset_workspace_ids = [
+    tfe_workspace.aws.id,
+    tfe_workspace.github.id,
+  ]
+
 }
